8cfe7583d3a45e9258bfaff468764ed1db0b73cd
Mahadev <project9162595119@gmail.com>
2025-05-29 09:37:45 +0530

    skaf

--- src/components/product-profile/ProductSpecifications.jsx
+++ src/components/product-profile/ProductSpecifications.jsx
@@ -44,11 +44,11 @@ const ProductSpecifications = () => {
   };
 
   return (
-    <div className="mx-auto px-2 sm:px-6 lg:px-8 py-12">
+    <div className="mx-auto sm:px-6 lg:px-8 py-12">
 
       {/* Tabs */}
-      <div className="border-b border-gray-200 mb-8">
-        <nav className="-mb-px flex space-x-8 overflow-x-scroll scroll">
+      <div className="border-b border-gray-200 mb-4">
+        <nav className="-mb-px flex space-x-4 overflow-x-scroll scroll">
           <button
             onClick={() => setActiveTab('specs')}
             className={`whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm ${activeTab === 'specs' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'}`}
